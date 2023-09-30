bcc0f9092eafed24799961194f91500b17c4831d
VacheGas <Vache.gasparyan3@gmail.com>
2023-09-30 21:34:44 +0400

    remove unused files

--- includes/Task.h
+++ includes/Task.h
@@ -2,13 +2,25 @@
 // Created by Vache Gasparyan on 23.09.23.
 //
 
-#ifndef TODO_LIST_TASK_H
-#define TODO_LIST_TASK_H
+#ifndef TODO_LIST_INCLUDES_TASK_H
+#define TODO_LIST_INCLUDES_TASK_H
+#include <QDate>
 
+struct Task {
+    QString name;
+    QString description;
+    QDate date;
+    bool state{};
 
-class Task {
-
+    Task() = default;
+    Task(QString newName,
+         QString newDescription,
+         QDate  newDate, bool newState)
+         : name (std::move(newName)),
+         description(std::move(newDescription)),
+         date(newDate),
+         state(newState) {}
 };
 
 
-#endif //TODO_LIST_TASK_H
+#endif //TODO_LIST_INCLUDES_TASK_H
--- src/filter_proxy_model.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Vache Gasparyan on 28.09.23.
-//
-
-#include "../includes/filter_model.h"
